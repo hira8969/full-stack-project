0d339ab23f1ef571fe0d07da44936a826db17d95
hira8969 <hk2493200@gmail.com>
2026-07-08 22:04:01 +0530

    add styleseet

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -2,11 +2,14 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.dao.UserRepositry;
 import com.example.entity.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HomeController {
@@ -18,19 +21,22 @@ public class HomeController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/test")
-    @ResponseBody
-    public String test() {
 
-        User user = new User();
-        user.setName("Hiralal Kumar");
-        user.setEmail("hk2493200@gmail.com");
-        user.setPassword("12345");
-        user.setRole("ROLE_USER");
-        user.setEnabled(true);
-
-        userRepository.save(user);
-
-        return "Working";
+        
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("title","Home- Smart Contact Manager");
+        return "home";
     }
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title","About- Smart Contact Manager");
+        return "about";
+    }
+    @GetMapping("/")
+    public String hello(Model model) {
+        return "home";
+    }
+    
+    
 }
