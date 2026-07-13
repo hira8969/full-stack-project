174c16cdc0b1fa86997a823b100d8b9c27e37c46
hira8969 <hk2493200@gmail.com>
2026-07-13 22:38:08 +0530

    add dataa

---
+++ src/main/java/com/example/config/CustomUserDetails.java
@@ -0,0 +1,8 @@
+package com.example.config;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+public class CustomUserDetails implements UserDetails {
+    
+}
