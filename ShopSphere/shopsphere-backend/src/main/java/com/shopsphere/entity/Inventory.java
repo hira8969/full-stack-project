7a7a76a6021e58553650e7854abd1d1dcf38f692
hira8969 <hk2493200@gmail.com>
2026-08-14 00:59:35 +0530

    Create Inventory.java

---
+++ ShopSphere/shopsphere-backend/src/main/java/com/shopsphere/entity/Inventory.java
@@ -0,0 +1,37 @@
+package com.shopsphere.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Inventory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false, unique = true)
+    private Product product;
+
+    @Column(name = "available_quantity", nullable = false)
+    @Builder.Default
+    private Integer availableQuantity = 0;
+
+    @Column(name = "reserved_quantity", nullable = false)
+    @Builder.Default
+    private Integer reservedQuantity = 0;
+
+    @Column(name = "sold_quantity", nullable = false)
+    @Builder.Default
+    private Integer soldQuantity = 0;
+
+    @Version
+    private Long version;
+}
