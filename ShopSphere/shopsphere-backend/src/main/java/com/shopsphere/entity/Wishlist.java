a10f1361d5cd199c9efa3fb1d444ed5f97a83c97
hira8969 <hk2493200@gmail.com>
2026-08-14 01:02:57 +0530

    Create Wishlist.java

---
+++ ShopSphere/shopsphere-backend/src/main/java/com/shopsphere/entity/Wishlist.java
@@ -0,0 +1,29 @@
+package com.shopsphere.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "wishlists")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Wishlist extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @OneToMany(mappedBy = "wishlist", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<WishlistItem> items = new ArrayList<>();
+}
