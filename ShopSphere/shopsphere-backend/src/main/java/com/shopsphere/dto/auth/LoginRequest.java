5cf31e831a78d4923a3fa87c6883bf2ff7c1aaca
hira8969 <hk2493200@gmail.com>
2026-08-14 00:59:58 +0530

    Update LoginRequest.java

--- ShopSphere/shopsphere-backend/src/main/java/com/shopsphere/dto/auth/LoginRequest.java
+++ ShopSphere/shopsphere-backend/src/main/java/com/shopsphere/dto/auth/LoginRequest.java
@@ -6,11 +6,10 @@ import lombok.Data;
 
 @Data
 public class LoginRequest {
-
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
+    @Email
+    @NotBlank
     private String email;
 
-    @NotBlank(message = "Password is required")
+    @NotBlank
     private String password;
 }
