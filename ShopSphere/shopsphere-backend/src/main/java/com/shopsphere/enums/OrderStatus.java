00543ce6b1c10d86bc9b3c18412f9d2f772b908c
hira8969 <hk2493200@gmail.com>
2026-08-14 01:02:40 +0530

    Create OrderStatus.java

---
+++ ShopSphere/shopsphere-backend/src/main/java/com/shopsphere/enums/OrderStatus.java
@@ -0,0 +1,14 @@
+package com.shopsphere.enums;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    PROCESSING,
+    PACKED,
+    SHIPPED,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+    CANCELLED,
+    RETURN_REQUESTED,
+    RETURNED
+}
