d0e00b4970da83c6f43625b77c616ca6498945ba
hira8969 <hk2493200@gmail.com>
2026-08-15 22:08:49 +0530

    Create ProductRepository.java

---
+++ ShopSphere/shopsphere-backend/src/main/java/com/shopsphere/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.shopsphere.repository;
+
+import com.shopsphere.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    Optional<Product> findBySku(String sku);
+
+    Page<Product> findBySellerProfileId(Long sellerProfileId, Pageable pageable);
+
+    Boolean existsBySku(String sku);
+}
