1bf979583ff6151d66e32fa8c7c4434c2a626cb1
hira8969 <hk2493200@gmail.com>
2026-08-15 22:08:26 +0530

    Create RoleRepository.java

---
+++ ShopSphere/shopsphere-backend/src/main/java/com/shopsphere/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.shopsphere.repository;
+
+import com.shopsphere.entity.Role;
+import com.shopsphere.enums.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleName name);
+}
