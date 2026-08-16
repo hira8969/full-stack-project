babbc1608fc1c5b26bda24661e9e6fd4df13174e
hira8969 <hk2493200@gmail.com>
2026-08-16 23:30:03 +0530

    Create CouponRepository.java

---
+++ ShopSphere/shopsphere-backend/src/main/java/com/shopsphere/repository/CouponRepository.java
@@ -0,0 +1,15 @@
+package com.shopsphere.repository;
+
+import com.shopsphere.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    Optional<Coupon> findByCode(String code);
+
+    Boolean existsByCode(String code);
+}
