5a820a7a862b5be09e0fd21617ae728e7b665b15
hira8969 <hk2493200@gmail.com>
2026-09-02 08:23:28 +0530

    Create foodservice.spec.ts

---
+++ BiteHub/frontend/src/app/services/food/foodservice.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { Foodservice } from './foodservice';
+
+describe('Foodservice', () => {
+  let service: Foodservice;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(Foodservice);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
