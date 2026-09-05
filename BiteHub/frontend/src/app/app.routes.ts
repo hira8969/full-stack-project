cafea5c37d479b736419c2bc14a141ee0534a3cc
hira8969 <hk2493200@gmail.com>
2026-09-05 19:20:35 +0530

    Update app.routes.ts

--- BiteHub/frontend/src/app/app.routes.ts
+++ BiteHub/frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Cart } from './cart/cart';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home
+  },
+   {
+    path: 'cart',
+    component: Cart
   }
 ];
